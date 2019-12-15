9c31ac898b7505d94e7311cca89fec05e351ec6f
forrest lyman <forrestlyman@gmail.com>
2019-12-15 14:52:55 -0800

    adds core app

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# AppStarter
+DigApp Starter
+==============
+
+
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.20.
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,27 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  digPlatform: {
+    log: {
+      console: {
+        enabled: true,
+        priority: ['ERROR', 'WARNING', 'INFO']
+      },
+      firestore: {
+        enabled: true,
+        priority: ['SYSTEM', 'ERROR']
+      }
+    },
+    firebase: {
+      apiKey: "AIzaSyD-f6LSqgZzkZO3WBG_gZ8d6z1-DYYlwV0",
+      authDomain: "dig-platform-app-sandbox.firebaseapp.com",
+      databaseURL: "https://dig-platform-app-sandbox.firebaseio.com",
+      projectId: "dig-platform-app-sandbox",
+      storageBucket: "dig-platform-app-sandbox.appspot.com",
+      messagingSenderId: "949411248539",
+      appId: "1:949411248539:web:bfc94735076f7f54d0c93e",
+      measurementId: "G-CJ49XZGY68"
+    }
+  }
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+import 'zone.js/dist/zone-error';  // Included with Angular CLI.
